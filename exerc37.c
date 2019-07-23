2c83a8b203524a7b10502d16e6ac0871da7fb1b6
Cezar Garrido <cezar.cgb18@outlook.com>
2019-07-23 20:54:58 -0300

    exercicio 37

---
+++ exerc37.c
@@ -0,0 +1,42 @@
+/*
+ * C Program to find whether a Number is Prime or Not using Recursion
+ */
+#include <stdio.h>
+ 
+int primeno(int, int);
+
+int main()
+{
+    int num, ehPrimo;
+    printf("Entre com um numero: ");
+    scanf("%d", &num);
+    ehPrimo = primeno(num, num / 2);
+    if (ehPrimo == 1)
+    {
+        printf("%d O numero eh primo\n", num);
+    }
+    else
+    {
+        printf("%d O numero nao eh primo\n", num);
+    }
+    return 0;
+}
+//checaPrimo: função recursiva que checa se um determinado número é primo
+int checaPrimo(int num, int i)
+{
+    if (i == 1)
+    {
+        return 1;
+    }
+    else
+    {  
+       if (num % i == 0) 
+       {
+         return 0;
+       }
+       else
+       {
+         return checaPrimo(num, i - 1);
+       }       
+    }
+}
